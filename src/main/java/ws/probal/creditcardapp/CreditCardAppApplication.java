e0e151bbfbf5ec38f9142b7c67464bdbb6cc79b1
probal <probal25@hotmail.com>
2024-01-05 22:54:36 +0600

    initial project setup

---
+++ src/main/java/ws/probal/creditcardapp/CreditCardAppApplication.java
@@ -0,0 +1,13 @@
+package ws.probal.creditcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/probal/creditcardapp/CreditCardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.probal.creditcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
